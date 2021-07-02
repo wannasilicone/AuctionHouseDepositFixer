929a847ea0e10169c8d08cdb17bbb4c7c69d6b02
eTzmNcbkrng <eTzmNcbkrng@users.noreply.github.com>
2021-07-03 01:31:08 +1200

    Fix issue of tattooing deposit with value of 1

--- AuctionHouseDepositFixer/AuctionHouseDepositFixer.lua
+++ AuctionHouseDepositFixer/AuctionHouseDepositFixer.lua
@@ -1,8 +1,14 @@
+local ChromieCraftDiscount, defaultDeposit, modifiedDeposit, roundedDeposit, finalDeposit
+
 hooksecurefunc("UpdateDeposit", function()
-	local ChromieCraftDiscount = 1 -- percentage of normal rates
-	local defaultDeposit = CalculateAuctionDeposit(AuctionFrameAuctions.duration, AuctionsStackSizeEntry:GetNumber() * AuctionsNumStacksEntry:GetNumber());
-	local modifiedDeposit = defaultDeposit * (ChromieCraftDiscount / 100);
-	local roundedDeposit = math.floor(modifiedDeposit);
-	local finalDeposit = roundedDeposit == 0 and 1 or roundedDeposit;
-	MoneyFrame_Update("AuctionsDepositMoneyFrame", finalDeposit);
-end);
+	ChromieCraftDiscount = 1 -- percentage of normal rates
+	defaultDeposit = CalculateAuctionDeposit(AuctionFrameAuctions.duration, AuctionsStackSizeEntry:GetNumber() * AuctionsNumStacksEntry:GetNumber())
+	modifiedDeposit = defaultDeposit * (ChromieCraftDiscount / 100)
+	if modifiedDeposit == 0 then
+		finalDeposit = 0
+	elseif modifiedDeposit < 1 then
+		finalDeposit = 1
+	else finalDeposit = math.floor(modifiedDeposit)
+		MoneyFrame_Update("AuctionsDepositMoneyFrame", finalDeposit)
+	end
+end)
